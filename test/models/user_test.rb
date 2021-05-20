bedcaef50e69a20540a6c14a42869d462dee0b68
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2021-05-20 17:58:56 +0800

    add first github workflow

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  # test "total_balance returns a users total balance of all wallets" do
+  #   user = User.new(first_name: "john", last_name: "lennon")
+  #   assert_equal "John Lennon", user.full_name
   # end
 end
