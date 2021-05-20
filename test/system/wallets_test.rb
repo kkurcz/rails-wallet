9c4759f0dbe8f993343257b6f94c297aed94fbda
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2021-05-20 17:41:27 +0800

    user test for sign in and create a new wallet

--- test/system/wallets_test.rb
+++ test/system/wallets_test.rb
@@ -8,4 +8,26 @@ class WalletsTest < ApplicationSystemTestCase
     save_and_open_screenshot
     assert_selector "h1", text: "Your Wallets"
   end
+  # test "displays correct amount of wallets" do
+  #   visit root_url
+  #   # opens a screenshot of test result
+  #   save_and_open_screenshot
+  #   assert_selector ".wallet-title", count: user.wallets.count
+  # end
+
+  test "lets a signed in user create a new wallet" do
+    login_as users(:kev)
+    visit "/wallets/new"
+    save_and_open_screenshot
+
+    fill_in "wallet_name", with: "Litecoin Wallet"
+    save_and_open_screenshot
+
+    click_on 'Create Wallet'
+    save_and_open_screenshot
+
+    # Should be redirected to show page with new product
+    assert_equal wallet_path(376363058), page.current_path
+    assert_selector "h5", text: "Litecoin Wallet"
+  end
 end
