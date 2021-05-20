edd236cd763d879aa5881bc4a974dd4991795925
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2021-05-20 20:29:01 +0800

    removed foreign key from transactions

--- db/migrate/20210520115501_create_transactions.rb
+++ db/migrate/20210520115501_create_transactions.rb
@@ -4,10 +4,12 @@ class CreateTransactions < ActiveRecord::Migration[6.0]
       t.integer :amount
       t.string :purpose
       t.string :type
-      t.references :sender_wallet, null: false, foreign_key: true
-      t.references :receiver_wallet, null: false, foreign_key: true
+      t.references :sender_wallet_id, null: false
+      t.references :receiver_wallet_id, null: false
 
       t.timestamps
     end
+    add_foreign_key :transactions, :wallets, column: :sender_wallet_id
+    add_foreign_key :transactions, :wallets, column: :receiver_wallet_id
   end
 end
