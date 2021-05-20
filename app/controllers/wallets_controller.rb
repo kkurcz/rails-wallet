28812c504bf4dac7d63694ec76f2be2c8ce9e00a
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2021-05-20 14:15:58 +0800

    created complete wallet actions

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,5 +1,6 @@
 class WalletsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
+  before_action :set_wallet, only: [:show, :edit, :update, :destroy]
   def index
     @wallets = Wallet.all
   end
@@ -9,11 +10,14 @@ class WalletsController < ApplicationController
 
   def new
     @wallet = Wallet.new
+    authorize @wallet
   end
 
+  # GET /wallet/1/edit
   def edit
   end
 
+  # POST /wallets
   def create
     @wallet = Wallet.new(wallet_params)
     @wallet.user = current_user
@@ -21,13 +25,30 @@ class WalletsController < ApplicationController
     if @wallet.save
       redirect_to @wallet, notice: 'Wallet was created.'
     else
-      render: new
+      render :new
     end
   end
 
+  # PATCH/PUT /walles/1
   def update
+    if @wallet.update(wallet_params)
+      redirect_to @wallet, notic: 'Wallet was updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @wallet.destroy
+    redirect_to wallets_url, notice: 'Wallet was deleted.'
+  end
+
+  private
+  def set_wallet
+    @wallet = Wallet.find(params[:id])
+  end
+
+  def wallet_params
+    params.require(:wallet).permit(:name)
   end
 end
