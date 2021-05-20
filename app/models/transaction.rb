36a363eae34d7f3f133054247536fe4f7293cf9b
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2021-05-20 20:09:33 +0800

    transaction model validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,8 @@
 class Transaction < ApplicationRecord
-  belongs_to :sender_wallet
-  belongs_to :receiver_wallet
+  belongs_to :sender_wallet, class_name: "Wallet"
+  belongs_to :receiver_wallet, class_name: "Wallet"
+
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :purpose, presence: true, length: { minimum: 1 }
+  validates :type, inclusion: { in: ["Withdrawal", "Deposit", "Transfer"] }
 end
