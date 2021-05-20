02b414295fde6ca3449825e5b513e5bf958d38a1
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2021-05-20 13:20:48 +0800

    Added wallet index

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,4 +1,6 @@
 class WalletsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
   def index
+    @wallets = Wallet.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'wallets/index'
   devise_for :users
-  root to: 'pages#home'
+  root to: 'wallets#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
