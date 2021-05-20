39a5b8e10e5ba63ff08199a4222a1d06b0e96141
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2021-05-20 13:46:27 +0800

    migrated db and updated wallet controller

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -3,4 +3,31 @@ class WalletsController < ApplicationController
   def index
     @wallets = Wallet.all
   end
+
+  def show
+  end
+
+  def new
+    @wallet = Wallet.new
+  end
+
+  def edit
+  end
+
+  def create
+    @wallet = Wallet.new(wallet_params)
+    @wallet.user = current_user
+
+    if @wallet.save
+      redirect_to @wallet, notice: 'Wallet was created.'
+    else
+      render: new
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
