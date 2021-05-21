c44b9fa6603092a4470452452a167f2ea9746bd9
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2021-05-21 14:39:51 +0800

    add transactions tests

--- test/models/wallet_test.rb
+++ test/models/wallet_test.rb
@@ -2,5 +2,16 @@ require 'test_helper'
 
 class WalletTest < ActiveSupport::TestCase
   def setup
+    @wallet = wallets(:kev_household_wallet)
+  end
+
+  test 'valid user' do
+    assert @wallet.valid?
+  end
+
+  test 'invalid without amount' do
+    @wallet.amount = nil
+    refute @wallet.valid?, 'saved wallet without an amount'
+    assert_not_nil @wallet.errors[:amount], 'no validation error for amount present'
   end
 end
