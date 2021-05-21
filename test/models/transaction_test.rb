ed2cec2029859e458af5821a3869a4083feade2b
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2021-05-21 12:58:58 +0800

    add user tests for invalid without email

--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -1,15 +1,22 @@
 require 'test_helper'
 
 class TransactionTest < ActiveSupport::TestCase
-  test "transfer money deducts and adds the correct amount from sender and receiver wallet" do
-    test_sender_wallet = Wallet.new(name: "Test Transfer Sender Wallet", balance: 500)
-    test_receiver_wallet = Wallet.new(name: "Test Transfer Receiver Wallet", balance: 200)
-    transaction = Transaction.new(activity: "Transfer", amount: "500", purpose: "Transfer money test", sender_wallet: test_sender_wallet, receiver_wallet: test_receiver_wallet)
-    # test_sender_wallet.balance == 0
-    # test_receiver_wallet.balance == 700
-  #   assert_equal "John Lennon", user.full_name
-    assert_equal( 700, test_receiver_wallet.balance )
-  end
+  # test "transfer money deducts correct amount from sender wallet" do
+  #   user = User.new(email: "lisa@example.com", password: "123456")
+  #   user.save!
+  #   test_sender_wallet = Wallet.new(name: "Test Transfer Sender Wallet", balance: 500, user_id: user)
+  #   test_sender_wallet.save!
+  #   test_receiver_wallet = Wallet.new(name: "Test Transfer Receiver Wallet", balance: 200, user_id: user)
+  #   test_receiver_wallet.save!
+  #   transfer_transaction = Transaction.new(activity: "Transfer", amount: "500", purpose: "Transfer money test", sender_wallet_id: test_sender_wallet, receiver_wallet_id: test_receiver_wallet)
+  #   transfer_transaction.save!
+  #   # test_sender_wallet.balance == 0
+  #   # test_receiver_wallet.balance == 700
+    # transfer_transaction = Transaction.new(activity: "Transfer", amount: "500", purpose: "Transfer money test", sender_wallet_id: test_sender_wallet, receiver_wallet_id: test_receiver_wallet)
+
+  # #   assert_equal "John Lennon", user.full_name
+  #   assert_equal( 0, test_sender_wallet.balance )
+  # end
 
   # def update_balance
   #   # raise
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,8 +1,17 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "total_balance returns a users total balance of all wallets" do
-  #   user = User.new(first_name: "john", last_name: "lennon")
-  #   assert_equal "John Lennon", user.full_name
-  # end
+  def setup
+    @user = users(:kev)
+  end
+
+  test 'valid user' do
+    assert @user.valid?
+  end
+
+  test 'invalid without email' do
+    @user.email = nil
+    refute @user.valid?, 'saved user without an email'
+    assert_not_nil @user.errors[:email], 'no validation error for email present'
+  end
 end
