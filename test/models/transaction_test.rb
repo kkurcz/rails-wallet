01124ba8e9e34b47924c78fa717972e676214d2b
Kevin Kurczseitz <kevinkurczseitz@icloud.com>
2021-05-24 23:04:04 +0800

    add test for funds_sufficient model method

--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -9,7 +9,9 @@ class TransactionTest < ActiveSupport::TestCase
     # .yml file for transactions didn't work due to sender and receiver wallet so wrote out the transactions here, should refactor:
     @deposit_transaction = Transaction.new(activity: "Deposit", amount: "500", purpose: "Transfer money test", sender_wallet: @wallet, receiver_wallet: @wallet)
     @withdrawal_transaction = Transaction.new(activity: "Withdrawal", amount: "500", purpose: "Transfer money test", sender_wallet: @wallet, receiver_wallet: @wallet)
-    @transfer_transaction = Transaction.new(id: 1, activity: "Transfer", purpose: "Transfer Test", amount: 500, sender_wallet: @receiver_wallet, receiver_wallet: @sender_wallet)
+    @transfer_transaction = Transaction.new(id: 1, activity: "Transfer", purpose: "Transfer Test", amount: 500, sender_wallet: @sender_wallet, receiver_wallet: @receiver_wallet)
+    @insufficient_funds_withdrawal = Transaction.new(id: 1, activity: "Withdrawal", purpose: "Transfer Test", amount: 2000, sender_wallet: @sender_wallet, receiver_wallet: @receiver_wallet)
+    @insufficient_funds_transfer = Transaction.new(id: 1, activity: "Transfer", purpose: "Transfer Test", amount: 2000, sender_wallet: @sender_wallet, receiver_wallet: @receiver_wallet)
   end
 
   test 'All transaction types are valid' do
@@ -25,13 +27,21 @@ class TransactionTest < ActiveSupport::TestCase
 
   test 'Withdraw money adds correct amount to wallet balance' do
     @withdrawal_transaction.update_balance
-    assert_equal 0, @withdrawal_transaction.receiver_wallet.balance, msg: "Withdraw money test failed, deposit did not add the correct amount to wallet balance"
+    assert_equal 0, @withdrawal_transaction.receiver_wallet.balance, msg: "Withdraw money test failed, withrdawal did not deduct the correct amount to wallet balance"
   end
 
   test 'Transfer money adds correct amount to receiver wallet balance and deducts correct amount from sender wallet balance' do
     @transfer_transaction.update_balance
     assert_equal 0, @transfer_transaction.sender_wallet.balance, msg: "Transfer money test failed, sender wallet did not deduct the correct amount from wallet balance"
     assert_equal 1000, @transfer_transaction.receiver_wallet.balance, msg: "Transfer money test failed, receiver wallet did not add the correct amount to wallet balance"
+  end
+
+  test 'Withdrawal will not be processed if funds insufficient' do
+    assert @insufficient_funds_withdrawal.funds_sufficient? == false, msg: "Withdrawal funds test failed, user can withdraw more money than wallet balance"
+  end
+
+  test 'Transfer will not be processed if funds insufficient' do
+    assert @insufficient_funds_transfer.funds_sufficient? == false, msg: "Transfer funds test failed, user can transfer more money than wallet balance"
   end
 end
 
